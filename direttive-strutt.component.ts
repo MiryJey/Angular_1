cfcce5c706b1e5e87bc3ef7bbf690f40700d06b2
Miriam <miriambnb91@gmail.com>
2024-10-20 14:14:14 +0200

    Add files via upload

--- direttive-strutt.component.ts
+++ direttive-strutt.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
+
 @Component({
   selector: 'app-direttive-strutt',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,BrowserModule],
   templateUrl: './direttive-strutt.component.html',
   styleUrl: './direttive-strutt.component.css'
 })
